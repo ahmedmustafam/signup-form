7fb9d91296ebba32b85843cf6682e2953d9548fb
Ahmed Mustafa <107986169+ahmedmustafam@users.noreply.github.com>
2025-02-15 18:54:54 +0400

    Clean up code and add documentation

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const password1 = document.getElementById("password");
 const password2 = document.getElementById("passwordconf");
 
+// function to match passwords
+
 password2.addEventListener("input", () => {
     if (password2.value != password1.value) {
         password2.setCustomValidity("Passwords do not match");
